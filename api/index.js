287dac513ef886aff7c4caca0b2487c76f8aab91
binqnb18 <hoangvu180597@gmail.com>
2024-11-05 23:11:40 +0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import mongoose  from 'mongoose';
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose
@@ -11,11 +13,14 @@ mongoose
     })
     .catch((err) => {
         console.log(err);
-    })
+    });
+
 const app = express();
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
-
-app.use("/api/user", userRoutes);
